e204b238aacafae46921bfc975e571ed5100b0cb
Eterst <jonder09@hotmail.com>
2022-03-16 14:42:32 -0600

    Co-authored-by: Roy8743 <Roy8743@users.noreply.github.com> Co-authored-by: JuanBautistaF  <Metanoyt@users.noreply.github.com> Co-authored-by: Francmata <Francmata@users.noreply.github.com>

--- preprocess.py
+++ preprocess.py
@@ -11,6 +11,7 @@ tokens = nltk.word_tokenize(archivo.read())
 
 def borrarStopwords():
     stop_words = set(stopwords.words('spanish'))
+    lemmatizer = WordNetLemmatizer()
 
     filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
 
@@ -19,21 +20,24 @@ def borrarStopwords():
     for w in tokens:
         if w not in stop_words:
             filtered_sentence.append(w)
-            diccionario[hashlib.md5(w.encode()).hexdigest()] = w
+            # Aqui se hace la lematización
+            punishedW = lemmatizer.lemmatize(w)
+            diccionario[hashlib.md5(punishedW.encode()).hexdigest()] = punishedW
     return filtered_sentence
-#ya ni se que hace la funcion JAJAJAJA
-def lemantizarTokens():
-    lemmatizer = WordNetLemmatizer()
-    listaLemantizada =[]
-    for w in tokens:
-        punishedW = lemmatizer.lemmatize(w)
-        if punishedW not in listaLemantizada:
-            listaLemantizada.append(punishedW)
+#ya ni se que hace la funcion
 
-    return listaLemantizada
+# Ya esta borrarStopWords tambien lematiza pero :v
+# deberians er funciones distintas no? :V o da igual :V 
+# Di no dijo nada nada mas dijo implementar
+
 
 def termSelect():
+    #TODO: Opcional
     print("a")
+
+def Tesauros():
+    #TODO
+    return
 
 sin_stopwords = borrarStopwords()
 
