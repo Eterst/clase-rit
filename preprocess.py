b7d6ff63044fa5fcacc19e3475b158a1cff42042
Eterst <jonder09@hotmail.com>
2022-03-16 13:50:55 -0600

    a

--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,29 @@
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 #Install NLTK: run: pip install --user -U nltk
 
-stop_words = set(stopwords.word('spanish'))
 
-archivo = open("EXTRA_23-2019,_28_NOV_17-12-2019_09_11_31.pdf.txt")
 
+archivo = open("EXTRA_23-2019,_28_NOV_17-12-2019_09_11_31.txt", encoding="utf8")
 tokens = nltk.word_tokenize(archivo.read())
 
-filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
- 
-filtered_sentence = []
- 
-for w in tokens:
-    if w not in stop_words:
-        filtered_sentence.append(w)
+def borrarStopwords():
+    stop_words = set(stopwords.words('spanish'))
+
+    filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
+    
+    filtered_sentence = []
+    
+    for w in tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+
+def lemantizarTokens():
+    lemmatizer = WordNetLemmatizer()
+
+def termSelect():
+    print("a")
 
 print(tokens[0:10])
