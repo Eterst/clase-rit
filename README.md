596687656d7ab2f4f5cbd9a8b2d8f8459387ba22
Eterst <jonder09@hotmail.com>
2022-03-17 20:08:38 -0600

    Co-authored-by: JuanBautistaF  <Metanoyt@users.noreply.github.com> Co-authored-by: Roy8743 <Roy8743@users.noreply.github.com> Co-authored-by: Francmata <Francmata@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # clase-rit
+pip install -U spacy
+python -m spacy download es_core_news_md
--- preprocess.py
+++ preprocess.py
@@ -1,45 +1,48 @@
+from tokenize import Token
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 import hashlib
+
 #Install NLTK: run: pip install --user -U nltk
-
-
 
 archivo = open("EXTRA_23-2019,_28_NOV_17-12-2019_09_11_31.txt", encoding="utf8")
 tokens = nltk.word_tokenize(archivo.read())
 
-def borrarStopwords():
+def borrarStopWords(lematize):
     stop_words = set(stopwords.words('spanish'))
-    lemmatizer = WordNetLemmatizer()
 
-    filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
-
+    #filtered_sentence = [w for w in tokens if not w.lower() in stop_words]
     diccionario = dict()
 
     for w in tokens:
         if w not in stop_words:
-            filtered_sentence.append(w)
-            # Aqui se hace la lematización
-            punishedW = lemmatizer.lemmatize(w)
-            diccionario[hashlib.md5(punishedW.encode()).hexdigest()] = punishedW
+            if lematize:
+                lematizeWord(diccionario,w)
+            else:
+                filtered_sentence.append(w)
+    
+    filtered_sentence = list(diccionario.values())
     return filtered_sentence
-#ya ni se que hace la funcion
 
-# Ya esta borrarStopWords tambien lematiza pero :v
-# deberians er funciones distintas no? :V o da igual :V 
-# Di no dijo nada nada mas dijo implementar
+def lematizeWord(diccionario, w):
+    lemmatizer = WordNetLemmatizer()
+    # Aqui se hace la lematización
+    punishedW = lemmatizer.lemmatize(w)
+    diccionario[hashlib.md5(punishedW.encode()).hexdigest()] = punishedW
 
-
-def termSelect():
-    #TODO: Opcional
-    print("a")
+def selection(tokens):
+    filtered_set = []
+    for w in tokens:
+        if len(w) > 7:
+            filtered_set.append(w)
+    return filtered_set
 
 def Tesauros():
-    #TODO
+    #TODO https://www.nltk.org/_modules/nltk/corpus/reader/lin.html
     return
 
-sin_stopwords = borrarStopwords()
+sin_stopwords = borrarStopWords(True)
 
 print(sin_stopwords[0:20])
 print("\n\n",tokens[0:20])
